8509f0ba49b9667b5337ad838ed8a679f5ecb67a
dgeison <dgeison.peixoto@gmail.com>
2023-04-18 23:07:22 -0300

    props

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar';
 
-export function Comment(props) {
+export function Comment({content}) {
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/dgeison.png" alt='' />
@@ -17,7 +17,7 @@ export function Comment(props) {
                             <Trash size={20} />
                         </button>
                     </header>
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                    <p>{content}</p>
                 </div>
                 <footer >
                     <button>
