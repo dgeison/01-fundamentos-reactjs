79c8880b84c28bb41f1fcb2e82d8a7549c29c022
dgeison <dgeison.peixoto@gmail.com>
2023-04-18 23:29:41 -0300

    insert key props

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,58 @@
-import { Post } from './components/Post'
-import { Header } from './components/Header'
-import { Sidebar } from './components/Sidebar'
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 
-
-import './global.css'
-import styles from './App.module.css'
+import "./global.css";
+import styles from "./App.module.css";
 
 const posts = [
   {
     id: 1,
     author: {
-      avatarUrl: 'https://github.com/dgeison.png',
-      name: 'Dgeison Peixoto',
-      role: 'Data Scientist'
+      avatarUrl: "https://github.com/dgeison.png",
+      name: "Dgeison Peixoto",
+      role: "Data Scientist",
     },
     content: [
-      { type: 'paragraph', content: '🐍💻📊 - para representar a linguagem Python e sua aplicação em análise de dados' },
-      { type: 'paragraph', content: ' - para representar a grande comunidade de desenvolvedores trabalhando juntos na melhoria da linguagem Python' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      {
+        type: "paragraph",
+        content:
+          "🐍💻📊 - para representar a linguagem Python e sua aplicação em análise de dados",
+      },
+      {
+        type: "paragraph",
+        content:
+          " - para representar a grande comunidade de desenvolvedores trabalhando juntos na melhoria da linguagem Python",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2023-04-17 22:30:00'),
+    publishedAt: new Date("2023-04-17 22:30:00"),
   },
   {
     id: 2,
     author: {
-      avatarUrl: 'https://avatars.githubusercontent.com/u/67557314?v=4',
-      name: 'Izadora Peixoto',
-      role: 'Desenvolvedora Back-end'
+      avatarUrl: "https://avatars.githubusercontent.com/u/67557314?v=4",
+      name: "Izadora Peixoto",
+      role: "Desenvolvedora Back-end",
     },
     content: [
-      { type: 'paragraph', content: '🐍💻📊 - para representar a linguagem Python e sua aplicação em análise de dados' },
-      { type: 'paragraph', content: ' - para representar a grande comunidade de desenvolvedores trabalhando juntos na melhoria da linguagem Python' },
-      { type: 'link', content: 'jane.design/doctorcare' },
+      {
+        type: "paragraph",
+        content:
+          "🐍💻📊 - para representar a linguagem Python e sua aplicação em análise de dados",
+      },
+      {
+        type: "paragraph",
+        content:
+          " - para representar a grande comunidade de desenvolvedores trabalhando juntos na melhoria da linguagem Python",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
     ],
-    publishedAt: new Date('2021-03-10 22:30:00'),
+    publishedAt: new Date("2021-03-10 22:30:00"),
   },
-]
-
+];
 
 export function App() {
-
   return (
     <div>
       <Header />
@@ -47,18 +60,18 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {posts.map(post => {
+          {posts.map((post) => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
               />
-            )
+            );
           })}
         </main>
       </div>
     </div>
-  )
+  );
 }
-
